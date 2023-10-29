887a4b49088501d56866194e53913076a2669edb
chibuzoanagbado <anagbadochibuzo@gmail.com>
2023-10-29 09:47:41 +0100

    created pages and configured navigation links

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(requests):
     return render(requests, 'pages/home.html')
+
+def about(requests):
+    return render(requests, 'pages/about.html')
+
+def services(requests):
+    return render(requests, 'pages/services.html')
+
+def contact(requests):
+    return render(requests, 'pages/contact.html')
